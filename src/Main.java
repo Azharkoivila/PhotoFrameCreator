16f87957c48a06c7c160bbf9d89db8fb9bc89c84
Azharkoivila <azharkoivila@gmail.com>
2022-11-28 19:49:03 +0530

    about panel added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Desktop;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -8,6 +9,8 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import javax.imageio.ImageIO;
@@ -16,6 +19,9 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -30,7 +36,7 @@ public class Main implements ActionListener{
 	int width=1000,height=1000;
 	String formant="jpg";
 	JLabel Imagelb;
-	
+	JMenuItem about;
 	public Main () {
 		jf=new JFrame("Frame Creator");
 		
@@ -50,6 +56,26 @@ public class Main implements ActionListener{
 		jf.setLayout(null);
 		
 		
+		
+		
+		
+		
+		JMenuBar Mb = new JMenuBar();
+		 JMenu help = new JMenu("Help");
+		 about = new JMenuItem("About");
+		Mb.setBackground(new Color(33,47,60,255));
+		help.setForeground(Color.white);
+		Mb.setOpaque(true);
+		help.add(about);
+		Mb.add(help);
+		jf.setJMenuBar(Mb);
+		about.addActionListener(this);
+		
+		
+		
+		
+		
+		
 		label=new JLabel("This is a Simple  app to Support Blood Donators");
 		label.setFont(new Font("Serif", Font.PLAIN, 20));
 		label.setBounds(65,300,500,50);	
@@ -59,7 +85,7 @@ public class Main implements ActionListener{
 		
 		buttonchoose=new JButton("Choose Image");
 		buttonchoose.setBounds(190,450,200,50);
-		buttonchoose.setBackground(new Color(250,104,0,255));
+		buttonchoose.setBackground(new Color(207, 10, 10));
 		buttonchoose.setForeground(Color.white);
 		buttonchoose.setVisible(true);
 		buttonchoose.addActionListener(this);
@@ -166,6 +192,15 @@ public class Main implements ActionListener{
 		
 		
 	        }
+	        
+	        if(e.getSource()==about) {
+	        	try {
+					about();
+				} catch (URISyntaxException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+	        }
 		}
 
 
@@ -239,7 +274,76 @@ public class Main implements ActionListener{
 		
 	}
 	
-	
+	 void about() throws URISyntaxException {
+		 JFrame help=new JFrame("About Us");
+		 help.setLayout(null);
+		 help.getContentPane().setBackground(new Color(17,17,17,255));
+		 help.setResizable(false);
+		 help.setBounds(650,250,250,250);
+		 
+		 
+		JLabel aboutlabel=new JLabel("Developed by");
+			aboutlabel.setBounds(0,10,200,20);
+			aboutlabel.setBackground(Color.blue);
+			aboutlabel.setForeground(Color.white);
+			aboutlabel.setFont(new Font("Arial",Font.ITALIC,17));
+			aboutlabel.setVisible(true);
+			help.add(aboutlabel);
+			
+			JLabel azharkoivila=new JLabel("Azharkoivila");
+			azharkoivila.setBounds(70,50,200,40);
+			azharkoivila.setForeground(Color.GREEN);
+			azharkoivila.setFont(new Font("Arial",Font.ITALIC,20));
+			azharkoivila.setVisible(true);
+			help.add(azharkoivila);
+			
+			final URI uri = new URI("http://azharkoivila.gitlab.io");
+			JButton button = new JButton();
+			button.setBounds(25,100,200,20);
+			button.setForeground(Color.BLUE);
+			button.setFont(new Font("Arial",Font.PLAIN,18));
+			button.setText("Contact Me");
+			button.setBorderPainted(false);
+			button.setOpaque(false);
+			button.setBackground(Color.BLUE);
+			help.add(button);
+			button.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					if (Desktop.isDesktopSupported()) {
+						Desktop desktop = Desktop.getDesktop();
+						 {
+							try {
+								desktop.browse(uri);
+							} catch (IOException e1) {
+								e1.printStackTrace();
+							}
+						}
+						}
+					
+					
+				}
+			});
+		
+			
+			
+			JLabel special=new JLabel("Frame Designed By");
+			special.setBounds(0,120,200,40);
+			special.setForeground(Color.white);
+			special.setFont(new Font("Arial",Font.ITALIC,17));
+			special.setVisible(true);
+			help.add(special);
+		 
+			JLabel haneefa=new JLabel("Haneefa Harish");
+			haneefa.setBounds(70,150,200,40);
+			haneefa.setForeground(Color.GREEN);
+			haneefa.setFont(new Font("Arial",Font.ITALIC,20));
+			haneefa.setVisible(true);
+			help.add(haneefa);
+			
+		button.setVisible(true);
+		help.setVisible(true);
+		 
+	 }
 		 
 		 
 }

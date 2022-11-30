e559b1fa9f5f08b4d2c665b0a13f6dd01813e843
Azharkoivila <azharkoivila@gmail.com>
2022-11-30 21:38:07 +0530

    code cleaned

--- src/Main.java
+++ src/Main.java
@@ -39,26 +39,14 @@ public class Main implements ActionListener{
 	JMenuItem about;
 	public Main () {
 		jf=new JFrame("Frame Creator");
-		
 		jf.setResizable(false);
-		
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jf.getContentPane().setBackground(new Color(17,17,17,255));
-		
-		
 		Imagelb=new JLabel(new ImageIcon(getClass().getClassLoader().getResource("banner.gif")));
-		
 		jf.getContentPane().add(Imagelb);
-		
 		jf.pack();
 		jf.setBounds(600,200,600,600);
-		
 		jf.setLayout(null);
-		
-		
-		
-		
-		
 		
 		JMenuBar Mb = new JMenuBar();
 		 JMenu help = new JMenu("Help");
@@ -70,11 +58,6 @@ public class Main implements ActionListener{
 		Mb.add(help);
 		jf.setJMenuBar(Mb);
 		about.addActionListener(this);
-		
-		
-		
-		
-		
 		
 		label=new JLabel("This is a Simple  app to Support Blood Donators");
 		label.setFont(new Font("Serif", Font.PLAIN, 20));
@@ -90,11 +73,6 @@ public class Main implements ActionListener{
 		buttonchoose.setVisible(true);
 		buttonchoose.addActionListener(this);
 		jf.add(buttonchoose);
-		
-		
-		 
-     
-		
 		jf.setVisible(true);
 	}
 	
@@ -108,100 +86,89 @@ public class Main implements ActionListener{
 		new Main();
 	}
 
-
-
-
-
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
 		
 	        if (e.getSource() == buttonchoose) {
 	            JFileChooser fileChooser = new JFileChooser();
-	            FileNameExtensionFilter filter = new FileNameExtensionFilter("JPEG", "jpeg", "jpg", "png", "bmp", "gif");
+	            FileNameExtensionFilter filter = new FileNameExtensionFilter("JPEG", "jpeg", "jpg", "png");
 	            fileChooser.addChoosableFileFilter(filter);
 	            fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
 	            int result = fileChooser.showOpenDialog(getParent());
 	            if (result == JFileChooser.APPROVE_OPTION) {
-	                
 	                    File file = fileChooser.getSelectedFile();
-	                    
-	                    
-	                   try {
+						try {
 							orginal = ImageIO.read(file);
-						} catch (IOException e2) {
+						} catch (IOException e1) {
 							// TODO Auto-generated catch block
-							e2.printStackTrace();
 							JOptionPane.showMessageDialog(null, "ERROR");
+							e1.printStackTrace();
 						}
+						
 	                    File compressed=new File("compressed.jpg");
 	                    BufferedImage resized=new BufferedImage(width, height, orginal.getType());
 	                    Graphics2D g2=resized.createGraphics();
 	                    g2.drawImage(orginal,0,0,width,height, null);
 	                    g2.dispose();
-	            		try {
+						try {
 							ImageIO.write(resized, formant, compressed);
-						} catch (IOException e4) {
+						} catch (IOException e1) {
 							// TODO Auto-generated catch block
-							e4.printStackTrace();
+							e1.printStackTrace();
 						}
-	                    
-	                    
-	        
-	             // source images
-	    		
+						// source images
 						try {
 							image1 = ImageIO.read(new File("compressed.jpg"));
-						} catch (IOException e3) {
+						} catch (IOException e1) {
 							// TODO Auto-generated catch block
-							e3.printStackTrace();
+							e1.printStackTrace();
 						}
-				
-				
 						try {
 							overlay = ImageIO.read((getClass().getClassLoader().getResource("frame.png")));
-						} catch (IOException e2) {
+						} catch (IOException e1) {
 							// TODO Auto-generated catch block
-							e2.printStackTrace();
+							e1.printStackTrace();
+						}
+						// create the new image
+						int w = Math.max(image1.getWidth(), overlay.getWidth());
+						int h = Math.max(image1.getHeight(), overlay.getHeight());
+						BufferedImage combined = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+						// paint both images, preserving the alpha channels
+						Graphics g = combined.getGraphics();
+						g.drawImage(image1, 0, 0, null);
+						g.drawImage(overlay, 0, 0, null);
+						g.dispose();
+						// Save
+						try {
+							ImageIO.write(combined, "PNG", new File("framed.jpg"));
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						try {
+							display();
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
 						}
 
-
-	    			// create the new image
-	    			int w = Math.max(image1.getWidth(), overlay.getWidth());
-	    			int h = Math.max(image1.getHeight(), overlay.getHeight());
-	    			BufferedImage combined = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-
-	    			// paint both images, preserving the alpha channels
-	    			Graphics g = combined.getGraphics();
-	    			g.drawImage(image1, 0, 0, null);
-	    			g.drawImage(overlay, 0, 0, null);
-
-	    			g.dispose();
-
-	    			// Save
-	    			try {
-						ImageIO.write(combined, "PNG", new File("framed.jpg"));
-						display();
-						
-					} catch (IOException e1) {
+	        }
+	        	
+	  }
+	        
+	        if(e.getSource()==about) {
+	        	
+					try {
+						about();
+					} catch (URISyntaxException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
-
-	        }
-		
-		
-	        }
-	        
-	        if(e.getSource()==about) {
-	        	try {
-					about();
-				} catch (URISyntaxException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+				
 				}
 	        }
-		}
+		
 
 
 
@@ -212,55 +179,28 @@ public class Main implements ActionListener{
 				        String path=file.getAbsolutePath();
 						ImageIcon imageIcon = new ImageIcon(bufferedImage);
 					    JFrame DisplyFrame = new JFrame("Preview");
-					        
 					    DisplyFrame.getContentPane().setBackground(new Color(17,17,17,255));
 					    //DisplyFrame.setResizable(false);
-					       
-					        
-					       
 					        JLabel jLabel = new JLabel();
 					        jLabel.setIcon(imageIcon);
-					        
 					        DisplyFrame.getContentPane().add(jLabel);
-					        
 					        DisplyFrame.pack();
 					        DisplyFrame.setBounds(200,200,1000,1050);
-					        
 					        DisplyFrame.setLayout(null);
-					        
-					        
-					             
-					        
 					        DisplyFrame.setVisible(true);
-					        
-					        
-					        
-					        
-					        
-					        
+					    
 					        JFrame pathjf=new JFrame();
-					        pathjf.setResizable(false);
-							
+					        pathjf.setResizable(false);							
 							pathjf.setBounds(100,100,700,100);
-							
 							pathjf.getContentPane().setBackground(new Color(17,17,17,255));
-							
-							
 							JLabel pathlb=new JLabel("Image Saved To:- "+path);
 							pathlb.setBounds(10,0,700,100);
 							pathlb.setForeground(Color.white);
 							pathlb.setVisible(true);
-							
 							pathjf.setLayout(null);
 							pathjf.add(pathlb);
 							pathjf.setVisible(true);
-							
-
-		
 					}
-
-
-
 
 	private Component getParent() {
 		// TODO Auto-generated method stub
@@ -280,7 +220,6 @@ public class Main implements ActionListener{
 		 help.getContentPane().setBackground(new Color(17,17,17,255));
 		 help.setResizable(false);
 		 help.setBounds(650,250,250,250);
-		 
 		 
 		JLabel aboutlabel=new JLabel("Developed by");
 			aboutlabel.setBounds(0,10,200,20);
@@ -312,20 +251,17 @@ public class Main implements ActionListener{
 					if (Desktop.isDesktopSupported()) {
 						Desktop desktop = Desktop.getDesktop();
 						 {
-							try {
-								desktop.browse(uri);
-							} catch (IOException e1) {
-								e1.printStackTrace();
-							}
+								try {
+									desktop.browse(uri);
+								} catch (IOException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
 						}
-						}
-					
-					
+					}
 				}
 			});
 		
-			
-			
 			JLabel special=new JLabel("Frame Designed By");
 			special.setBounds(0,120,200,40);
 			special.setForeground(Color.white);
@@ -339,13 +275,12 @@ public class Main implements ActionListener{
 			haneefa.setFont(new Font("Arial",Font.ITALIC,20));
 			haneefa.setVisible(true);
 			help.add(haneefa);
-			
-		button.setVisible(true);
-		help.setVisible(true);
+
+			button.setVisible(true);
+			help.setVisible(true);
 		 
 	 }
-		 
-		 
+		 	 
 }
 		 
 		 
